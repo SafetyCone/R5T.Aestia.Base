303abdba1508c4d1611117278e2a9b6fb38cf6a8
MinexAutomation <minex.automation@gmail.com>
2020-01-05 12:35:06 -0800

    Changed to asynchronous methods.

--- source/R5T.Aestia.Base/Code/Services/Interfaces/IAnomalyRepository.cs
+++ source/R5T.Aestia.Base/Code/Services/Interfaces/IAnomalyRepository.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 using R5T.Corcyra;
 using R5T.Francia;
@@ -14,47 +15,47 @@ namespace R5T.Aestia
         /// <summary>
         /// Creates a new anomaly identity.
         /// </summary>
-        AnomalyIdentity New();
+        Task<AnomalyIdentity> New();
 
 
-        void SetReportedUTC(AnomalyIdentity anomaly, DateTime dateTime);
+        Task SetReportedUTC(AnomalyIdentity anomaly, DateTime dateTime);
 
-        DateTime GetReportedUTC(AnomalyIdentity anomaly);
+        Task<DateTime> GetReportedUTC(AnomalyIdentity anomaly);
 
 
         /// <summary>
         /// Adds an image file to the anomaly.
         /// There can be multiple images per anomaly.
         /// </summary>
-        void AddImageFile(AnomalyIdentity anomaly, ImageFileIdentity imageFile);
+        Task AddImageFile(AnomalyIdentity anomaly, ImageFileIdentity imageFile);
 
-        IEnumerable<ImageFileIdentity> GetImageFiles(AnomalyIdentity anomalyIdentity);
+       Task<IEnumerable<ImageFileIdentity>> GetImageFiles(AnomalyIdentity anomalyIdentity);
 
 
         /// <summary>
         /// An anomaly has only one reported location.
         /// </summary>
-        void SetReportedLocation(AnomalyIdentity anomaly, LocationIdentity reportedLocation);
+        Task SetReportedLocation(AnomalyIdentity anomaly, LocationIdentity reportedLocation);
 
-        LocationIdentity GetReportedLocation(AnomalyIdentity anomaly);
+        Task<LocationIdentity> GetReportedLocation(AnomalyIdentity anomaly);
 
 
-        void SetReporterLocation(AnomalyIdentity anomaly, LocationIdentity reporterLocation);
+        Task SetReporterLocation(AnomalyIdentity anomaly, LocationIdentity reporterLocation);
 
         /// <summary>
         /// An anomaly might not have a reporter location. Perhaps the reporter turned off location services!
         /// </summary>
-        bool HasReporterLocation(AnomalyIdentity anomaly, out LocationIdentity reporterLocation);
+        Task<(bool HasReporterLocation, LocationIdentity LocationIdentity)> HasReporterLocation(AnomalyIdentity anomaly);
 
-        LocationIdentity GetReporterLocation(AnomalyIdentity anomaly);
+        Task<LocationIdentity> GetReporterLocation(AnomalyIdentity anomaly);
 
 
-        bool ExistsTextItem(AnomalyIdentity anomaly, TextItemTypeIdentity textItemType);
+        Task<bool> ExistsTextItem(AnomalyIdentity anomaly, TextItemTypeIdentity textItemType);
 
-        void SetTextItem(AnomalyIdentity anomaly, TextItemTypeIdentity textItemType, TextItemIdentity textItem);
+        Task SetTextItem(AnomalyIdentity anomaly, TextItemTypeIdentity textItemType, TextItemIdentity textItem);
 
-        TextItemIdentity GetTextItem(AnomalyIdentity anomaly, TextItemTypeIdentity textItemType);
+        Task<TextItemIdentity> GetTextItem(AnomalyIdentity anomaly, TextItemTypeIdentity textItemType);
 
-        IEnumerable<Tuple<TextItemTypeIdentity, TextItemIdentity>> GetTextItems(AnomalyIdentity anomaly);
+        Task<IEnumerable<Tuple<TextItemTypeIdentity, TextItemIdentity>>> GetTextItems(AnomalyIdentity anomaly);
     }
 }
