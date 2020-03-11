d22a16edc9e0001646e03a6569793656e6c8b000
MinexAutomation <minex.automation@gmail.com>
2020-03-11 04:32:33 -0700

    Added an AnomalyLocation class (mostly a network message type).

---
+++ source/R5T.Aestia.Base/Code/Classes/AnomalyLocation.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+using R5T.Corcyra;
+
+
+namespace R5T.Aestia
+{
+    public class AnomalyLocation
+    {
+        public Guid Identity { get; set; }
+        public LngLat Location { get; set; }
+    }
+}
