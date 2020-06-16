49e583c0a1a34580f3f1ef92ed721ac0bb4dacae
MinexAutomation <minex.automation@gmail.com>
2020-06-16 16:45:35 -0700

    Added IAnomalyRepository extension method to add an Anomaly only if it doesn't exist.

---
+++ source/R5T.Aestia.Base/Code/Services/Extensions/IAnomalyRepositoryExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using R5T.Sindia;
+
+namespace R5T.Aestia
+{
+    public static class IAnomalyRepositoryExtensions
+    {
+        public static async Task AddOnlyIfNotExistsAsync(this IAnomalyRepository anomalyRepository, AnomalyIdentity anomalyIdentity)
+        {
+            var exists = await anomalyRepository.ExistsAsync(anomalyIdentity);
+            if (!exists)
+            {
+                await anomalyRepository.AddAsync(anomalyIdentity);
+            }
+        }
+    }
+}
