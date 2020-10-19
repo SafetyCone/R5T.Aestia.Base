36c9bc6c7f2f0dad2e31f6d57f06cc98ddfdcacf
Tasha Arvanitis <tasha.arvanitis@gmail.com>
2020-10-19 13:45:15 -0500

    Allow multiple catchments per anomaly

--- source/R5T.Aestia.Base/Code/Classes/AnomalyInfo.cs
+++ source/R5T.Aestia.Base/Code/Classes/AnomalyInfo.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 using R5T.Corcyra;
 using R5T.Francia;
@@ -13,13 +14,13 @@ namespace R5T.Aestia
     {
         public AnomalyIdentity AnomalyIdentity { get; set; }
         public DateTime ReportedUTC { get; set; }
-        public CatchmentIdentity CatchmentIdentity { get; set; }
+        public List<CatchmentIdentity> CatchmentIdentities { get; set; }
         public List<ImageFileIdentity> ImageFileIdentities { get; set; }
         public LocationIdentity ReportedLocation { get; set; }
         public LocationIdentity ReporterLocation { get; set; }
         public List<TextItemIdentity> TextItems { get; set; }
 
-        public bool HasCatchment => this.CatchmentIdentity is object;
+        public bool HasCatchment => this.CatchmentIdentities.Any();
         public bool HasReportedLocation => this.ReportedLocation is object;
         public bool HasReporterLocation => this.ReporterLocation is object;
     }
